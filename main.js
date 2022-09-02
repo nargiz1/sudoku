894fb46492f73dfe9d0ca57deaeda99e9bc90c01
Nargiz Ramazanova <nargiz.rp@code.local>
2022-09-02 09:48:50 +0400

    frst

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 let cells = document.querySelectorAll("td:not(.given-number)")
 let candidates = document.querySelectorAll(".candidates");
 let values = document.querySelectorAll(".value");
-let givenNums = document.querySelectorAll(".given-number");
 let nums = document.querySelectorAll(".number-control");
 let btn = document.querySelector("#candidate-switch");
 
@@ -12,23 +11,25 @@ cells.forEach(cell => {
         if( document.querySelector(".selected")){
             document.querySelector(".selected").classList.remove("selected");
         }
-        cell.classList.toggle("selected");
+        cell.classList.add("selected");
         activeCell = cell;
     })
 })
 
 nums.forEach(num => {
     num.addEventListener("click", function(){
-        if(btn.checked === false){
-            activeCell.lastElementChild.innerText = num.innerText;
-        }else if(btn.checked === true){
-            if(activeCell.firstElementChild.innerText.includes(num.innerText)){
-                let str = activeCell.firstElementChild.innerText.replace(num.innerText, '');
-                activeCell.firstElementChild.innerText = str;
-            }else{
-                let str = activeCell.firstElementChild.innerText + num.innerText;
-                let arr = Array.from(str).sort();
-                activeCell.firstElementChild.innerText = arr.join('');
+        if(activeCell != undefined){
+            if(btn.checked === false){
+                activeCell.lastElementChild.innerText = num.innerText;
+            }else if(btn.checked === true){
+                if(activeCell.firstElementChild.innerText.includes(num.innerText)){
+                    let str = activeCell.firstElementChild.innerText.replace(num.innerText, '');
+                    activeCell.firstElementChild.innerText = str;
+                }else{
+                    let str = activeCell.firstElementChild.innerText + num.innerText;
+                    let arr = Array.from(str).sort();
+                    activeCell.firstElementChild.innerText = arr.join('');
+                }
             }
         }
     })
